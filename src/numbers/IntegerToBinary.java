7da1607c85ebcbaaa418df536932b8d2baa2e462
lppduy <lephamphuongduy@gmail.com>
2024-06-22 15:28:53 +0700

    numbers: Integer to binary

---
+++ src/numbers/IntegerToBinary.java
@@ -0,0 +1,22 @@
+package numbers;
+
+import java.util.Scanner;
+
+public class IntegerToBinary {
+    public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int[] binary = new int[10];
+        int i = 0;
+        
+        while (number > 0) {
+            binary[i++] = number % 2;
+            number = number / 2;
+        }
+        
+        for (int j = i - 1; j >= 0; j--) {
+            System.out.print(binary[j]);
+        }
+    }
+}
